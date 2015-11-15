2f9935ab2b3de295970a344143e94facbb8db3cc
Alan Plotko <alan.plotko@gmail.com>
2015-11-15 10:50:17 -0500

    Add new feature for quick release and data refresh
    - Releases are now quick and possible for Linux, Windows, and OSX.
    - Data can be refreshed via the navigation.
    - Release icon updated with StoryHub logo.

---
+++ build/app.js
@@ -0,0 +1,20 @@
+(function () { 'use strict';
+
+	// Here is the starting point for code of your own application.
+	// All stuff below is just to show you how it works. You can delete all of it.
+
+	// Node.js modules and those from npm
+	// are required the same way as always.
+	var os = require('os');
+	var app = require('remote').require('app');
+	var jetpack = require('fs-jetpack').cwd(app.getAppPath());
+
+	// Holy crap! This is browser window with HTML and stuff, but I can read
+	// here files like it is node.js! Welcome to Electron world :)
+	console.log(jetpack.read('package.json', 'json'));
+
+	// window.env contains data from config/env_XXX.json file.
+	var envName = window.env.name;
+
+})();
+//# sourceMappingURL=app.js.map
